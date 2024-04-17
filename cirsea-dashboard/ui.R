475935e5b29fa911c26cb38dc8a7161342530a31
Chloe <chloe.swick@gmail.com>
2024-04-16 22:32:55 -0700

    update action buttons, icons, info tabs, and theme

--- cirsea-dashboard/ui.R
+++ cirsea-dashboard/ui.R
@@ -221,7 +221,7 @@ body <- dashboardBody(
             # how to use
             fluidRow(
               column(width = 12,
-              div("Click on an IUU Type to learn more.", style = "margin-bottom: 20px; padding:50x")
+              div("Click on an IUU fishing type to learn more", style = "margin-bottom: 20px; padding:50x; font-size: 24px;")
             )),
     
             # IUU type text fluidRow ----
@@ -239,71 +239,74 @@ body <- dashboardBody(
               #             class = "btn btn-default action-button",
               #             icon("dumbbell"), "Underreporting of", br(), "fishing effort"),
               # 
-              
+            
               
 
              # Make a box for the IUU type buttons
-              box(width = 4,
+              box(width = 5, style = "display: flex; flex-wrap: wrap; justify-content: center;",
                      # Define buttons for IUU types dynamically or statically here
-                     actionButton("Dark Vessels (not broadcasting location via VMS/AIS)", 
-                                  HTML(paste(icon("moon"),
-                                             "<br>Dark Vessels")), 
-                                  style='width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Fishing above quota",
-                                  HTML(paste(icon("weight-scale"), 
-                                             "<br>Fishing above quota")), 
-                                  style='width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Fishing in a prohibited zone", 
-                                  HTML(paste(icon("ban"), 
-                                             "<br>Fishing in a prohibited zone")), 
-                                 style='width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Fishing out of season", 
+                   actionButton("Fishing out of season", 
                                   HTML(paste(icon("earth-americas"), 
                                              "<br>Fishing out of season")), 
-                                 style='width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Fishing without license", 
-                                  HTML(paste(icon("id-card"), 
-                                             "<br>Fishing without license")), 
-                                 style='width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Gear-related offense", 
-                                  HTML(paste(icon("gear"), 
-                                             "<br>Gear-related offense")), 
-                                 style='width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Transhipments", 
-                                  HTML(paste(icon("ship"),
-                                             "<br>Illegal Transhipments")), 
-                                 style='width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Underreporting of fishing catch", 
+                                 style='width:200px; height:100px; font-size:120%; white-space:normal;'),
+                   actionButton("Fishing in a prohibited zone", 
+                                  HTML(paste(icon("ban"), 
+                                             "<br>Fishing in a prohibited zone")), 
+                                 style='width:200px; height:100px; font-size:120%; white-space:normal;'),
+                   actionButton("Fishing above quota",
+                                  HTML(paste(icon("weight-scale"), 
+                                             "<br>Fishing above quota")), 
+                                  style='width:200px; height:100px; font-size:120%; white-space:normal;'),
+                   actionButton("Underreporting of fishing catch", 
                                   HTML(paste(icon("fish"),
                                              "<br>Underreporting of fishing catch")), 
-                                  style='width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Underreporting of fishing effort", 
+                                  style='width:200px; height:100px; font-size:120%; white-space:normal;'),
+                   actionButton("Underreporting of fishing effort", 
                                   HTML(paste(icon("dumbbell"),
                                              "<br>Underreporting of fishing effort")), 
-                                  style='width:200px; height:150px; font-size:120%; white-space:normal;'),
+                                  style='width:200px; height:100px; font-size:120%; white-space:normal;'),
+                  actionButton("Prohibited Species", 
+                               HTML(paste(icon("fish-fins"),
+                                          '<br>Fishing for prohibited species')), 
+                               style='width:200px; height:100px; font-size:120%; white-space:normal;'),
+                  actionButton("Bycatch", 
+                               HTML(paste(icon("shrimp"),
+                                          '<br>Illegal bycatch and discard')), 
+                               style='width:200px; height:100px; font-size:120%; white-space:normal;'),
+                   actionButton("Gear-related offense", 
+                                  HTML(paste(icon("gear"), 
+                                             "<br>Gear-related offense")), 
+                                 style='width:200px; height:100px; font-size:120%; white-space:normal;'),
+                   actionButton("Fishing without license", 
+                                  HTML(paste(icon("id-card"), 
+                                             "<br>Fishing without license")), 
+                                 style='width:200px; height:100px; font-size:120%; white-space:normal;'),
+                   actionButton("Transhipments", 
+                                  HTML(paste(icon("ship"),
+                                             "<br>Illegal Transhipments")), 
+                                 style='width:200px; height:100px; font-size:120%; white-space:normal;'),
+                   actionButton("Dark Vessels (not broadcasting location via VMS/AIS)", 
+                                  HTML(paste(icon("moon"),
+                                             "<br>Dark Vessels")), 
+                                  style='width:200px; height:100px; font-size:120%; white-space:normal;'),
                      actionButton("Vessel with false flag", 
                                   HTML(paste(icon("flag"),
                                              '<br>Vessel with false flag')), 
-                                 style='width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Prohibited Species", 
-                                  HTML(paste(icon("fish-fins"),
-                                             '<br>Fishing for prohibited species')), 
-                                  style='width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Bycatch", 
-                                  HTML(paste(icon("shrimp"),
-                                             '<br>Illegal bycatch and discard')), 
-                                  style='width:200px; height:150px; font-size:120%; white-space:normal;'),
+                                 style='width:200px; height:100px; font-size:120%; white-space:normal;'),
                      actionButton("Reporting", 
                                   HTML(paste(icon("book"),
                                              '<br>Noncompliance with reporting requirements')), 
-                                  style='width:200px; height:150px; font-size:120%; white-space:normal;')
+                                  style='width:400px; height:100px; font-size:120%; white-space:normal;')
                      # Add more buttons as needed
                      
               ), # end box for iuu type buttons
-            
+           
             # Make a box for the IUU Type Text
-              box(width = 8,
+              box(width = 7,
                      uiOutput("iuu_text"), # Placeholder for displaying text based on the selected IUU type
+                  tags$img(src = "boat_1.jpeg",
+                           alt = "boats",
+                           style = "max-width: 100%;") 
               ) # End box for iuu type text
         
             ) # End fluid row
@@ -316,8 +319,8 @@ body <- dashboardBody(
             # Add new boxes in this section to structure like IUU types
             fluidRow(
               column(width = 12,
-              div("Click on a Monitoring Strategy to learn more.", style = "margin-bottom: 20px;"),
-              div("Sensors", style = "margin-bottom: 20px; font-weight: bold;", style = "margin-bottom: 20px;")
+              div("Click on a monitoring strategy to learn more", style = "margin-bottom: 20px; font-size: 24px;"),
+              div("Sensors", style = "margin-bottom: 20px; font-weight: bold;", style = "margin-bottom: 20px; font-size: 20px;")
               
             )),
             
@@ -325,55 +328,56 @@ body <- dashboardBody(
             # Sensors fluidRow ----
             fluidRow(
               
-              box(width = 4,
+              box(width = 5,style = "display: flex; flex-wrap: wrap; justify-content: center;",
                      # Define buttons for sensors
-                     actionButton("hydroacoustics", 
-                                  HTML(paste(icon("ear-listen"),
-                                             "<br>Hydroacoustics")), 
-                                  style = 'width:200px; height:150px; font-size:120%; white-space:normal;'),
+                     actionButton("AIS/VMS", 
+                                  HTML(paste(icon("location-crosshairs"),
+                                             "<br>AIS and VMS")),
+                                  style = 'width:200px; height:100px; font-size:120%; white-space:normal;'),
                      actionButton("long_range_camera", 
                                   HTML(paste(icon("camera"),
                                              "<br>Cameras")), 
-                                  style = 'width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("onboard_observer", 
-                                  HTML(paste(icon("glasses"),
-                                             "<br>Onboard Observers")), 
-                                  style = 'width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("radar",
-                                  HTML(paste(icon("satellite-dish"),
-                                             "<br>Radar")), 
-                                  style = 'width:200px; height:150px; font-size:120%; white-space:normal;'),
+                                  style = 'width:200px; height:100px; font-size:120%; white-space:normal;'),
+                     actionButton("Electronic Monitoring Systems", 
+                                  HTML(paste(icon("video"),
+                                             "<br>Electronic Monitoring Systems")),
+                                  style = 'width:200px; height:100px; font-size:120%; white-space:normal;'),
+                    actionButton("Satellites", 
+                                  HTML(paste(icon("satellite"),
+                                             "<br>Satellites")),
+                                  style = 'width:200px; height:100px; font-size:120%; white-space:normal;'),
+                     actionButton("hydroacoustics", 
+                                  HTML(paste(icon("ear-listen"),
+                                             "<br>Hydroacoustics")), 
+                                  style = 'width:200px; height:100px; font-size:120%; white-space:normal;'),
                      actionButton("radio_frequency", 
                                   HTML(paste(icon("radio"),
                                              "<br>Radio Frequency")),
-                                  style = 'width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("AIS/VMS", 
-                                  HTML(paste(icon("radio"),
-                                             "<br>AIS and VMS")),
-                                  style = 'width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Electronic Monitoring Systems", 
-                                  HTML(paste(icon("radio"),
-                                             "<br>Electronic Monitoring Systems")),
-                                  style = 'width:200px; height:150px; font-size:120%; white-space:normal;'),
-                     actionButton("Satellites", 
-                                  HTML(paste(icon("radio"),
-                                             "<br>Satellites")),
-                                  style = 'width:200px; height:150px; font-size:120%; white-space:normal;'),
+                                  style = 'width:200px; height:100px; font-size:120%; white-space:normal;'),
+                     actionButton("onboard_observer", 
+                                  HTML(paste(icon("glasses"),
+                                             "<br>Onboard Observers")), 
+                                  style = 'width:200px; height:100px; font-size:120%; white-space:normal;'),
+                     actionButton("radar",
+                                  HTML(paste(icon("satellite-dish"),
+                                             "<br>Radar")), 
+                                  style = 'width:200px; height:100px; font-size:120%; white-space:normal;'),
+                    
                      # Add more buttons as needed
               ),
            
             
       
             
-              box(width = 8,
+              box(width = 7,
                      uiOutput("sensor_text")) # Placeholder for displaying text based on the selected IUU type
             ), # END Sensors  fluidRows
     
             
             # Platforms and how to use
             fluidRow(
-              column(width = 12,
-              div("Platforms", style = "margin-bottom: 20px; margin-top: 20px; font-weight: bold;", style = "margin-bottom: 20px;")
+              column(width = 12, 
+              div("Platforms", style = "margin-bottom: 20px; margin-top: 20px; font-weight: bold;", style = "margin-bottom: 20px; font-size: 20px;")
               
             )),
           
@@ -381,38 +385,39 @@ body <- dashboardBody(
             # Platforms fluidRow ----
             fluidRow(
               
-              box(width = 4,
+              box(width = 5, style = "display: flex; flex-wrap: wrap; justify-content: center;",
                      # Define buttons for platforms
                      actionButton("aerial_drone", 
                                   HTML(paste(icon("helicopter"),
-                                             "<br>Aerial Drone<br>")), 
-                                  style = 'width:200px; height:150px; font-size:120%; white-space: normal;'),
+                                             "<br>UAV<br>")), 
+                                  style = 'width:200px; height:100px; font-size:120%; white-space: normal;'),
+                    actionButton("usv",
+                                  HTML(paste(icon("water"),
+                                             "<br>USV")), 
+                                  style = 'width:200px; height:100px; font-size:120%'),
                      actionButton("manned_aircraft", 
                                   HTML(paste(icon("plane"), 
                                              "<br>Manned Aircraft")), 
-                                  style = 'width:200px; height:150px; font-size:120%'),
+                                  style = 'width:200px; height:100px; font-size:120%'),
                      actionButton("manned_vessel", 
                                   HTML(paste(icon("ship"),
                                              "<br>Manned Vessel")), 
-                                  style = 'width:200px; height:150px; font-size:120%'),
-                     actionButton("on_shore_command_center", 
-                                  HTML(paste(icon("computer"),
-                                             "<br>Onshore Command Center")), 
-                                  style = 'width:200px; height:150px; font-size:120%; white-space: normal;'),
+                                  style = 'width:200px; height:100px; font-size:120%'),
                      actionButton("smart_buoy", 
                                   HTML(paste(icon("code-commit"),
                                              "<br>Smart Buoy")), 
-                                  style = 'width:200px; height:150px; font-size:120%'),
-                     actionButton("usv",
-                                  HTML(paste(icon("water"),
-                                             "<br>USV")), 
-                                  style = 'width:200px; height:150px; font-size:120%'),
+                                  style = 'width:200px; height:100px; font-size:120%'),
+                     actionButton("on_shore_command_center", 
+                                  HTML(paste(icon("computer"),
+                                             "<br>Onshore Command Center")), 
+                                  style = 'width:200px; height:100px; font-size:120%; white-space: normal;'),
+                    
                      # Add more buttons as needed
               ),
            
             
             
-              box(width = 8, 
+              box(width = 7, 
                      uiOutput("platform_text")) # Placeholder for displaying text based on the selected IUU type
             ) # END Platforms fluidRows
             
